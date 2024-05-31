469ed5afe6a68f62f46d44f80f8a80fb7218dd88
MchevezS <saboriomichelle93@gmail.com>
2024-05-31 14:49:33 -0600

    le di la funcion click al  boton agregar tarea

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 // Inserte el código aquí
 
+//aqui voy a validar el boton de agregar tarea //
+let btnAgregarTarea = document.getElementById("btnAgregarTarea")
+btnAgregarTarea.addEventListener('click', function () {
+    
+    alert('Le has dado click al btnagregartarea')
+}) 
