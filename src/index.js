d65542f9b7ff97ace4e2f4d2c792447c62336804
MchevezS <saboriomichelle93@gmail.com>
2024-05-31 15:59:28 -0600

    cambios en las funciones de dar click

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-// Inserte el código aquí
+// Aqui voy a Insertar todas las variables
+let btnAgregarTarea = document.getElementById("btnAgregarTarea")
+let inputtareas = document.getElementById("inputtareas")
+
+
+
+// aqui voy a validar el input de donde voy a ingresar el texto o las tareas//
+inputtareas.addEventListener('click', function () {
+    alert('has escrito una tarea')
+})
+   
+
 
 //aqui voy a validar el boton de agregar tarea //
-let btnAgregarTarea = document.getElementById("btnAgregarTarea")
 btnAgregarTarea.addEventListener('click', function () {
-    
-    alert('Le has dado click al btnagregartarea')
+     alert('Le has dado click al btnagregartarea')
 }) 
