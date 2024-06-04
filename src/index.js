4a0eaa99455d0dc50a0bc466f49a690b93e3b849
MchevezS <saboriomichelle93@gmail.com>
2024-06-04 09:35:11 -0600

    si mi input no tiene nada no se puede  agregar tareas

--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ let inputtareas = document.getElementById("inputtareas")
 
 
 // aqui voy a validar el input de donde voy a ingresar el texto o las tareas//
-inputtareas.addEventListener('click', function () {
-    alert('has escrito una tarea')
-})
-   
-
+function validarInput() {
+    let valorInput= document.getElementById("inputtareas").value;
+    if (valorInput == "") {
+       alert('porfavor agrega una tarea')
+       return false;
+    }
+    return true;
+} 
 
 //aqui voy a validar el boton de agregar tarea //
 btnAgregarTarea.addEventListener('click', function () {
-     alert('Le has dado click al btnagregartarea')
+    validarInput()
 }) 
+
+// lo que agregue en el input se me agregue en  la parte de abajo del input//
